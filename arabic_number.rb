aae57c1b2438dea73cb0c6d1ab4bc6af0787f1d8
Jakub Niewczas <niewczas.jakub@gmail.com>
2015-07-30 19:31:40 +0200

    proper translation for numbers (0..9)

--- arabic_number.rb
+++ arabic_number.rb
@@ -1,6 +1,8 @@
 class ArabicNumber < Struct.new(:number)
+  BASIC_NUMBERS = %w(zero one two three four five six seven eight nine)
+
   def to_english
-    return 'one' if number == 1
+    return BASIC_NUMBERS[number] if number.between?(0, 9)
     return 'eleven' if number == 11
     return 'one hundred' if number == 100
   end
--- arabic_number_spec.rb
+++ arabic_number_spec.rb
@@ -1,9 +1,6 @@
 require './arabic_number.rb'
 
 RSpec.describe ArabicNumber do
-  it 'returns proper translations for 1' do
-    expect(ArabicNumber.new(1).to_english).to eq 'one'
-  end
 
   it 'returns proper translations for 11' do
     expect(ArabicNumber.new(11).to_english).to eq 'eleven'
@@ -11,5 +8,11 @@ RSpec.describe ArabicNumber do
 
   it 'returns proper translations for 100' do
     expect(ArabicNumber.new(100).to_english).to eq 'one hundred'
+  end
+
+  it 'returns proper translations for numbers 0-9' do
+    ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].each_with_index do |v, i|
+      expect(ArabicNumber.new(i).to_english).to eq v
+    end
   end
 end
