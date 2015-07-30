4edfe4b748a01063768b60cd3f3ffa861d2c6f49
Jakub Niewczas <niewczas.jakub@gmail.com>
2015-07-30 19:35:25 +0200

    proper translation for numbers (10..19)

--- arabic_number.rb
+++ arabic_number.rb
@@ -1,9 +1,10 @@
 class ArabicNumber < Struct.new(:number)
   BASIC_NUMBERS = %w(zero one two three four five six seven eight nine)
+  TEENS_NUMBERS = %w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
 
   def to_english
     return BASIC_NUMBERS[number] if number.between?(0, 9)
-    return 'eleven' if number == 11
+    return TEENS_NUMBERS[number-10] if number.between?(10, 19)
     return 'one hundred' if number == 100
   end
 end
--- arabic_number_spec.rb
+++ arabic_number_spec.rb
@@ -2,10 +2,6 @@ require './arabic_number.rb'
 
 RSpec.describe ArabicNumber do
 
-  it 'returns proper translations for 11' do
-    expect(ArabicNumber.new(11).to_english).to eq 'eleven'
-  end
-
   it 'returns proper translations for 100' do
     expect(ArabicNumber.new(100).to_english).to eq 'one hundred'
   end
@@ -13,6 +9,22 @@ RSpec.describe ArabicNumber do
   it 'returns proper translations for numbers 0-9' do
     ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].each_with_index do |v, i|
       expect(ArabicNumber.new(i).to_english).to eq v
+    end
+  end
+
+  it 'returns proper translations for numbers 10-19' do
+    {10 => 'ten',
+     11 => 'eleven',
+     12 => 'twelve',
+     13 => 'thirteen',
+     14 => 'fourteen',
+     15 => 'fifteen',
+     16 => 'sixteen',
+     17 => 'seventeen',
+     18 => 'eighteen',
+     19 => 'nineteen'
+    }.each do |k, v|
+      expect(ArabicNumber.new(k).to_english).to eq v
     end
   end
 end
